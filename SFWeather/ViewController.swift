3528e325b7ce732c36aae01c915b4a8222d02c17
Joshua Harris <joshua.jharris13@gmail.com>
2015-11-14 22:43:24 -0500

    completed tut

--- SFWeather/ViewController.swift
+++ SFWeather/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var weatherCondition: UILabel!
+    var ref = Firebase(url: "https://sfweather-ios.firebaseio.com/")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ref.observeEventType(.Value, withBlock: { snapshot in self.weatherCondition.text = snapshot.value as? String
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateWeather(sender: UIButton) {
+        ref.setValue(sender.titleLabel?.text)
+    }
 
 }
 
